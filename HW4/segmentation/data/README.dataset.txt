d7fd8586cdb5645b7374e7dac4935415707cc974
Jackiempty <candypaper0123@gmail.com>
2026-05-01 20:01:21 +0800

    feat (HW4): Add segmentation part

---
+++ HW4/segmentation/data/README.dataset.txt
@@ -0,0 +1,6 @@
+# Jackiempty seg > 2026-05-01 7:02pm
+https://universe.roboflow.com/n26140692s-workspace/jackiempty-seg
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
---
+++ HW4/segmentation/train.py
@@ -0,0 +1,11 @@
+import os
+from ultralytics import YOLO
+
+if __name__ == "__main__":
+    # Get the absolute path of the directory containing train.py
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    # Pass an absolute path for data.yaml to prevent YOLO from losing track of it due to different execution paths
+    data_yaml_path = os.path.join(base_dir, "data", "data.yaml")
+    
+    model = YOLO("yolo26n-seg.pt")
+    model.train(data=data_yaml_path, epochs=100, batch=8)
